eaa856291ab29768d4bf6fef2022f73cb4cf8557
swarna <kumariswarna083@gmail.com>
2023-03-24 15:24:44 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './components1/ExpenseItem';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>This is my first react app</h2>
+        <ExpenseItem></ExpenseItem>
+      
+    </div>
+  );
+}
+
+export default App;
