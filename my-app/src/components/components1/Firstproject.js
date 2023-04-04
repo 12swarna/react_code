1191dd2e1eacfedfbf23523f780311a846cd5c63
swarna <kumariswarna083@gmail.com>
2023-04-04 11:13:43 +0530

    adding_ref

--- my-app/src/components/components1/Firstproject.js
+++ my-app/src/components/components1/Firstproject.js
@@ -1,68 +1,67 @@
-import { useState } from 'react';
-import './Firstproject.css'
-const Firstproject=( props)=>{
-    const[firstusername,setfirstusername]=useState("");
-    const[agedetails,setagedetails]=useState("");
-    const[isValid,setisvalid]=useState("true");
+import { useRef, useState } from 'react';
+import './Firstproject.css';
 
-    const usernamehandler=(event)=>{
-        setfirstusername(event.target.value)
+import Wrapper from './Wrapper';
+
+const Firstproject = (props) => {
+  const [isValid, setIsValid] = useState(false);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const collegeInputRef=useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const name = nameInputRef.current.value;
+    const age = ageInputRef.current.value;
+    const college=collegeInputRef.current.value;
+    console.log(name,age,college);
+
+    if (name.trim().length === 0 || age.trim().length === 0 || college.trim().length ===0) {
+      window.alert('Please enter a valid username,age and collegename');
+      setIsValid(true);
+      return;
     }
-    const agehandler=(event)=>{
-        setagedetails(event.target.value);
+
+    if (age <= 0) {
+      window.alert('Please enter an age greater than 0');
+      setIsValid(true);
+      return;
     }
-    const submithandler=(event)=>{
-        event.preventDefault();
-        if(firstusername.trim().length === 0 || agedetails.trim().length === 0 ){
-            window.alert("please enter valid username and age");
-            setisvalid(true);
-            return;
+     //making list//
+    const list = document.createElement('li');
+    list.className = 'list';
+    const unorderedList = document.getElementById('ul');
+    unorderedList.appendChild(list);
+    list.innerText = `${name} (${age} years old) ${college}`;
 
-        }
-        if(agedetails.trim() <=0){
-            window.alert("please enter age <=0");
-            return;
-        }
+    setIsValid(false);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    collegeInputRef.current.value='';
+  };
 
-        
-        const form={
-            username:firstusername,
-            age:agedetails
-
-        }
-        console.log(`${form.username} ${form.age} years old `);
-         
-        //create list//
-        let list=document.createElement("li");
-        list.className="list";
-        let unoderedlist=document.getElementById("ul");
-        unoderedlist.appendChild(list);
-        list.innerText=`${form.username} (${form.age} years old )`;
-        
-       
-
-    }
-    return(
-        <div className='thisdiv'>
-        <form className="firstform" onSubmit={submithandler}>
-           <div className={`firstdiv ${!isValid ? 'invalid' :" "}`} >
-            <label className='user'  >USERNAME</label>
-            <input className="user1" type="text" onChange={usernamehandler} />
-
-            </div>
-          <div className='seconddiv'>
-            <label className='user2'  >Age(years old)</label>
-            <input className="user2again" type="number" onChange={agehandler} />
-
-            </div>
-            <button type="submit" className='butto'>Add Users</button>
-           
-        </form>
-        <ul id="ul"></ul>
-        
+  return (
+    <Wrapper className='thisdiv'>
+      <form className='firstform' onSubmit={submitHandler}>
+        <div className={`firstdiv ${isValid ? 'invalid' : ''}`}>
+          <label className='user'>Username</label>
+          <input className='user1' type='text' ref={nameInputRef} />
         </div>
+        <div className={`middlediv ${isValid ? 'invalid' : ''}`}>
+            <label className='college_name'>COLLEGE NAME</label>
+            <input type="text" className='college' ref={collegeInputRef}></input>
+        </div>
+        <div className={`seconddiv ${isValid ? 'invalid' : ''}`}>
+          <label className='user2'>Age (years old)</label>
+          <input className='user2again' type='number' ref={ageInputRef} />
+        </div>
+        <button type='submit' className='button'>
+          Add User
+        </button>
+      </form>
+      <ul id='ul'></ul>
+    </Wrapper>
+  );
+};
 
-    );
-
-}
 export default Firstproject;
