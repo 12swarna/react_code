9b555816b8c7bef36932ee4e0e90cd083246ca7d
swarna <kumariswarna083@gmail.com>
2023-05-15 12:18:45 +0530

    adding_new_appo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,38 @@
-import ExpenseItem from './components1/ExpenseItem';
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+// Components for different "pages"
+const Home = () => <h1>Welcome to the Home Page!</h1>;
+const About = () => <h1>About Us</h1>;
+const Contact = () => <h1>Contact Us</h1>;
+
+const App = () => {
   return (
-    <div>
-      <h2>This is my first react app</h2>
-        <ExpenseItem></ExpenseItem>
-      
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
